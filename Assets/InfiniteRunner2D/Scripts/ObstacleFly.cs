56bab51de4d1dca4b32dab1e9d74a7b57b72896d
Your Name <you@example.com>
2019-03-27 13:36:41 +0300

    Add project

---
+++ Assets/InfiniteRunner2D/Scripts/ObstacleFly.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ObstacleFly : Obstacle
+{
+    private Transform _transform;
+    private Vector2 _startPosition;
+
+    [SerializeField]
+    private float _maxSpeed;
+
+    private void OnEnable()
+    {
+        _transform = transform;
+
+        _startPosition = _transform.position;
+        _startPosition.x += _maxSpeed / 2;
+    }
+
+    private void Update()
+    {
+        //_transform.position = new Vector3(transform.position.x, _startPosition.y + Mathf.Sin(_maxSpeed * Time.time), transform.position.z);
+        _transform.position = new Vector3( _startPosition.x + Mathf.Sin(_maxSpeed * Time.time), transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/InfiniteRunner2D/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform _transform;
+    private Rigidbody2D _rigidbody;
+
+    [SerializeField]
+    private Transform _groundCheck;
+    [SerializeField]
+    private float moveSpeed;
+
+    private void Start()
+    {
+        _transform = transform;
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            _rigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, _rigidbody.velocity.y);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            _rigidbody.velocity = new Vector2(0f, _rigidbody.velocity.y);
+        }
+      
+    }
+}
